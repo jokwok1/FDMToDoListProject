650d4e217a2b54d02e7ffde0bc68abf32d0c26e9
jacky tan 1 <jackypryo@hotmail.com>
2024-05-20 16:17:59 +0800

    Add project files.

---
+++ FDMToDoListProjectWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FDMToDoListProjectWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
